35abc756d6296e70e080057ed49294e3707b21a9
rizjaise <rizjaise@gmail.com>
2026-05-23 07:54:57 +0530

    Fixed subnet app creation issue

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "app" {
     name                 = "SpecterSubnetApp-Dev"
     resource_group_name  = var.resource_group_name
     virtual_network_name = azurerm_virtual_network.main.name
-    address_prefixes     = ["10.0.2.0/24 "]
+    address_prefixes     = ["10.0.2.0/24"]
     }
       
 resource "azurerm_subnet" "data" {
